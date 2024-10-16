b7e96125562f88f976228f6881fd7dc3e944d779
Mert çelikol <74824635+mertcelikol@users.noreply.github.com>
2024-10-16 23:32:48 +0000

    done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,18 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        console.log(`${p}${a}${n}.com`); // Concatenate strings and print the domain
+      }
+    }
+  }
 };
